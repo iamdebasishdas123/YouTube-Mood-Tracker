47482432605ef14cb8294fa2da6fd4b807ca2288
Debasish Das <iamdebasishdas123@gmail.com>
2026-05-25 23:49:06 +0530

    add params

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -85,7 +85,7 @@ def main():
         params = load_params(params_path='params.yaml')
         test_size = params['data_ingestion']['test_size']
         
-        df = load_data(data_url='https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
+        df = load_data(data_url='https://raw.githubusercontent.com/Himanshu-1703/reddit-sentiment-analysis/refs/heads/main/data/reddit.csv')
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=42)
         save_data(train_data, test_data, data_path='./data')
